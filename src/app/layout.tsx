12a6bad9988428b0165e95206d63cba7df26e364
Gabriel Cristiano Ferraez <134634881+GabrielFerrarez19@users.noreply.github.com>
2026-08-12 08:16:32 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: {
-    default: "ERP para Pequenas Empresas: Estoque e Vendas | ProTrack",
+    default: "ERP para Empresas: Estoque e Vendas | ProTrack",
     template: "%s | ProTrack",
   },
   description:
